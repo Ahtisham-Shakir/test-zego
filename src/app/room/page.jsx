0136e298d01cfa4fb804b63b3eb1edc537881b92
Ahtisham-Shakir <ahtishamshakir777@gmail.com>
2023-11-29 20:54:42 +0500

    fixed build

--- src/app/room/page.jsx
+++ src/app/room/page.jsx
@@ -1,39 +1,62 @@
-"use client"
-import React ,{useEffect} from 'react'
-import { ZegoUIKitPrebuilt } from "@zegocloud/zego-uikit-prebuilt"
+"use client";
+
+import React, { useEffect } from "react";
+
 const RoomPage = () => {
-    const roomID = "123"
+  const roomID = "123";
 
-    const myMeeting = async (element)=>{
-        const appID  = 2142330120
-        const serverSecret = "2d597e6ab5fa94e32e49b2e5c14db343"
-        const kitToken =  ZegoUIKitPrebuilt.generateKitTokenForTest(appID, serverSecret, roomID, Date.now().toString() ,  "Wahab");
-        const zp = ZegoUIKitPrebuilt.create(kitToken);
-        zp.joinRoom({
-           container: element,
-           sharedLinks: [
-                  {
-                    name: 'Personal link',
-                    url:
-                    window.location.protocol + '//' + 
-                    window.location.host + window.location.pathname +
-                      '?roomID=' +
-                      roomID,
-                  },
+  useEffect(() => {
+    const loadZegoUIKitPrebuilt = async () => {
+      try {
+        const { ZegoUIKitPrebuilt } = await import(
+          "@zegocloud/zego-uikit-prebuilt"
+        );
+
+        const element = document.getElementById("your-element-id");
+
+        if (element) {
+          const appID = 2142330120;
+          const serverSecret = "2d597e6ab5fa94e32e49b2e5c14db343";
+          const kitToken = ZegoUIKitPrebuilt.generateKitTokenForTest(
+            appID,
+            serverSecret,
+            roomID,
+            Date.now().toString(),
+            "Wahab"
+          );
+          const zp = ZegoUIKitPrebuilt.create(kitToken);
+          zp.joinRoom({
+            container: element,
+            sharedLinks: [
+              {
+                name: "Personal link",
+                url:
+                  window.location.protocol +
+                  "//" +
+                  window.location.host +
+                  window.location.pathname +
+                  "?roomID=" +
+                  roomID,
+              },
             ],
-           scenario: {
-                  mode: ZegoUIKitPrebuilt.GroupCall, // To implement 1-on-1 calls, modify the parameter here to [ZegoUIKitPrebuilt.OneONoneCall].
+            scenario: {
+              mode: ZegoUIKitPrebuilt.GroupCall,
             },
-            
-        });  
+          });
+        }
+      } catch (error) {
+        console.error("Error loading ZegoUIKitPrebuilt:", error);
+      }
+    };
 
-    }
+    loadZegoUIKitPrebuilt();
+  }, [roomID]);
 
   return (
     <div>
-        <div ref={myMeeting} className=""/>
+      <div id="your-element-id" className="" />
     </div>
-  )
-}
+  );
+};
 
-export default RoomPage
+export default RoomPage;
